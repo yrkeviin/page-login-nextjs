5d7d55533bf052f3feb689deba3375713fee82b8
yrkeviin <kevineziquiellima160@gmail.com>
2025-03-13 09:02:46 -0300

    Feat: Image perfil and containers

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,9 @@ export default function Profile() {
     return (
         <div className={styles.container}>
             <div className={styles.container_profile}>
-                <div className={styles.foto_perfil}></div>
+                <div className={styles.foto_perfil}>
+                    <img src="/kevinmotoperfil.png" alt="" />
+                </div>
             </div>
         </div>
     )
